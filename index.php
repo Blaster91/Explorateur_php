59f3fafb6559248a922fd3a1883d6966b6e4d83e
Romain Leroy <r.leroy@codeur.online>
2019-10-08 10:37:57 +0200

    création des fichiers

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Explorateur php</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Explorateur de fichiers PHP</h1>
+    </header>
+    <section>
+      
+    </section>
+  </body>
+</html>
