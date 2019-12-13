d653cab29584e5963acad6f31b63a27ad99c9237
Romain Leroy <r.leroy@codeur.online>
2019-12-13 18:59:11 +0100

    add html

--- index.php
+++ index.php
@@ -4,13 +4,246 @@
     <meta charset="utf-8">
     <title>Explorateur php</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
     <header>
       <h1>Explorateur de fichiers PHP</h1>
     </header>
     <section>
-      
+      <h2>index</h2>
+      <div class="container_table">
+        <table class="table_explorer">
+          <thead>
+            <tr>
+              <th>
+                nom
+              </th>
+              <th>
+                action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                explorer.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                adobe.exe
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+      <div class="popup">
+      </div>
     </section>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6fee70888d.js"></script>
   </body>
 </html>
