4c6f7e930a172b2eb5ec813af1d95c73c911d4b7
Romain Leroy <r.leroy@codeur.online>
2019-12-15 18:14:24 +0100

    add read files function

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+        $d = dir(".");
+        echo "Pointeur: ".$d->handle."<br>\n";
+ ?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
@@ -7,246 +12,54 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+
+
+    <!-- header -->
     <header>
-      <h1>Explorateur de fichiers PHP</h1>
-    </header>
-    <section>
-      <h2 class="mb-5">index /</h2>
+      <h1>Explorateur de fichiers   <img class="logo_php" src="img/php.png" alt="logo php"></h1>
+
+    </header><!-- header -->
+
+<div class="test">
+
+</div>
+    <!-- table -->
+    <section id="table_explorer">
+      <h2 class="mb-3"><?php echo "path: ".$d->path."<br>"; ?></h2>
 <div class="content_container_table">
   <div class="table_move">
-    <a href="#"><i class="fas fa-arrow-left"></i> Retour </a>
-    <a href="#">Précédent<i class="fas fa-arrow-right"></i></a>
+    <a class="link_table_move link_table_move_left" href="#"><i class="fas fa-arrow-left"></i> Retour </a>
+    <div class="space_bar">
+    </div>
+    <a class="link_table_move link_table_move_right" href="#">Précédent<i class="fas fa-arrow-right"></i></a>
   </div>
   <div class="container_table">
     <table class="table_explorer">
       <thead>
-        <tr>
-          <th>
-            nom
-          </th>
-          <th>
-            action
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            explorer.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            adobe.exe
-          </td>
-          <td>
-            <a href="#">supprimé</a>
-          </td>
-        </tr>
+        <?php
+        while($entry = $d->read()) {
+            echo "<tr>
+              <td>" . $entry . '
+              </td>
+              <td>
+                <a href="#">supprimé</a>
+              </td>
+            </tr>';
+        }
+        $d->close();
+         ?>
+
+
+
+
 
       </tbody>
     </table>
   </div>
 </div>
-
-      <div class="popup">
-      </div>
-    </section>
+<div class="popup">
+</div>
+    </section><!-- table -->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
